7988d5d1af1e08f075bd19915e1a2937090c6bb2
amaanraaj <amandwivedi00@gmail.com>
2023-09-27 01:37:12 +0530

    accordian completed with checkbox

--- Accordian/src/App.jsx
+++ Accordian/src/App.jsx
@@ -1,18 +1,37 @@
 import './index.css'
 import Accordian from './components/Accordian'
 import questions from './assets/data'
+import { useState } from 'react'
 
 function App() {
+  const [openId,setOpenId] = useState(null);
+  const [multiple,setMultiple] = useState(true)
+
+  const setOpenAccordianId = (id)=>{
+    setOpenId(multiple?null:id);
+  }
+
+  const toggleMultiple = ()=>{
+    if(multiple){
+      setOpenId(-1)
+    }
+    setMultiple(!multiple);
+  }
+
   return (
-    <>
+    <div className='p-10'>
+      <label>Multiple Allowed? </label>
+      <input type='checkbox' checked={multiple} onChange={toggleMultiple} ></input>
         {
           questions.map((question)=>(
             <Accordian 
               key={question.id}
+              openId = {openId}
+              setOpenAccordianId = {setOpenAccordianId}
               {...question} />
           ))
         }  
-    </>
+    </div>
   )
 }
 
--- Accordian/src/components/Accordian.jsx
+++ Accordian/src/components/Accordian.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Accordian = ({id,title,info}) => {
+const Accordian = ({id,title,info,openId,setOpenAccordianId}) => {
+  const [show,setShow] = useState(false);
+  const toggleSign = ()=>{
+    setShow(!show);
+    setOpenAccordianId(id);
+  }
+  useEffect(()=>{
+    if(openId){
+      setShow(openId===id)
+    }
+  },[openId])
+
   return (
-    <div>
+    <div className=' p-2 m-5 bg-slate-500 text-white text-xl border-2 border-black'>
       <div className='flex justify-between'>
-        <h2 className=''>{title}</h2>
-        <button>+</button>
+        <h2 className='font-serif'>{title}</h2>
+        <button onClick={toggleSign}>
+          {
+            show ? '-' : '+'
+          }
+        </button>
       </div>
-        <p>{info}</p>
+      {
+        show? <p className='font-mono text-base my-3'>{info}</p> : ''
+      }
+        
     </div>
   )
 }
