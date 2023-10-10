7145662cf441cac54da5b2c6a961e294050a3d4e
amaanraaj <amandwivedi00@gmail.com>
2023-10-10 22:59:29 +0530

    working

--- table-colorizer/src/App.jsx
+++ table-colorizer/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colorArray, setColorArray] = useState([]);
+
+  const handleColor = (e)=>{
+    console.log(e)
+  }
 
   return (
     <>
+      <div>
+        <input type='number' className='border-2 border-black'></input>
+        <button className='border-2 border-black' onClick={handleColor}>Color </button>
+        <button className='border-2 border-black'>Clear All</button>
+      </div>
       <div className="grid grid-cols-3 gap-4">
         {
           Array(9).fill("").map((e,index)=>
