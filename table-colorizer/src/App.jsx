a83a34aa6b0d6672aa07500d63e7e83a7e161cdb
amaanraaj <amandwivedi00@gmail.com>
2023-10-11 01:20:27 +0530

    completed colorizer

--- table-colorizer/src/App.jsx
+++ table-colorizer/src/App.jsx
@@ -2,26 +2,34 @@ import { useState } from 'react'
 
 function App() {
   const [colorArray, setColorArray] = useState([]);
+  const [currValue,setCurrValue] = useState();
 
   const handleColor = (e)=>{
-    console.log(e)
+    if(currValue>9 || currValue<1){
+      alert("Enter Valid number")
+    }
+    else if(colorArray.indexOf(currValue)==-1){
+      setColorArray((prev)=>[...prev,currValue]);
+    }   
   }
 
   return (
-    <>
+    <div className='flex flex-col items-center m-20'>
       <div>
-        <input type='number' className='border-2 border-black'></input>
-        <button className='border-2 border-black' onClick={handleColor}>Color </button>
-        <button className='border-2 border-black'>Clear All</button>
+        <input type='number' className='border-2 border-black px-2 py-1 rounded-xl' onChange={(e)=>(setCurrValue(parseInt(e.target.value)))} value={currValue}></input>
+        <button className='border-2 border-black mx-2 px-2 py-1 bg-slate-600 text-white rounded-xl' onClick={handleColor}>Color </button>
+        <button className='border-2 border-black px-2 py-1 bg-slate-600 text-white rounded-xl' onClick={()=>(setColorArray([]))}>Clear All</button>
       </div>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 m-5 w-60 h-40">
         {
           Array(9).fill("").map((e,index)=>
-            <div className='border-2'>{index+1}</div>
+            <div className={`${
+              colorArray.indexOf(index+1)!='-1'?'bg-red-600 text-white':'bg-white text-black'}
+               border border-black flex justify-center items-center `} key={index}>{index+1}</div>
           )
         }
       </div>
-    </>
+    </div>
   )
 }
 
